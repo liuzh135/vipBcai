9dafe7061f6bae7732c80d659ad069655fbff897
liuzh135 <626787193@qq.com>
2018-09-05 11:07:43 +0800

    commit create vipBcai system

---
+++ src/defaultSettings.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "navTheme": "dark",
+  "primaryColor": "#1890FF",
+  "layout": "sidemenu",
+  "contentWidth": "Fluid",
+  "fixedHeader": true,
+  "autoHideHeader": true,
+  "fixSiderbar": true,
+  "collapse": true
+};
---
+++ src/pages/Dashboard/models/monitor.js
@@ -0,0 +1,29 @@
+/* eslint-disable linebreak-style */
+import { queryTags } from '@/services/api';
+
+export default {
+  namespace: 'monitor',
+
+  state: {
+    tags: [],
+  },
+
+  effects: {
+    *fetchTags(_, { call, put }) {
+      const response = yield call(queryTags);
+      yield put({
+        type: 'saveTags',
+        payload: response.list,
+      });
+    },
+  },
+
+  reducers: {
+    saveTags(state, action) {
+      return {
+        ...state,
+        tags: action.payload,
+      };
+    },
+  },
+};
