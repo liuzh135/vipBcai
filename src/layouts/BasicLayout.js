63e1e1391f0efa51625740b1f21550eac1192840
liuzh135 <626787193@qq.com>
2018-09-11 10:41:01 +0800

    add menu user&form

--- src/layouts/BasicLayout.js
+++ src/layouts/BasicLayout.js
@@ -159,13 +159,13 @@ class BasicLayout extends React.PureComponent {
       }
     });
     if (!currRouterData) {
-      return 'Ant Design Pro';
+      return 'vipBocai';
     }
     const message = formatMessage({
       id: currRouterData.locale || currRouterData.name,
       defaultMessage: currRouterData.name,
     });
-    return `${message} - Ant Design Pro`;
+    return `${message} - vipBocai`;
   };
 
   getLayoutStyle = () => {
--- src/layouts/UserLayout.js
+++ src/layouts/UserLayout.js
@@ -2,8 +2,10 @@ import React, { Fragment } from 'react';
 import Link from 'umi/link';
 import { Icon } from 'antd';
 import GlobalFooter from '@/components/GlobalFooter';
+import { formatMessage } from 'umi/locale';
 import styles from './UserLayout.less';
 import logo from '../assets/logo.svg';
+import DocumentTitle from 'react-document-title';
 
 const links = [
   {
@@ -41,25 +43,52 @@ class UserLayout extends React.PureComponent {
   //   return title;
   // }
 
+
+  getPageTitle = pathname => {
+    let currRouterData = null;
+    // match params path
+    // Object.keys(this.breadcrumbNameMap).forEach(key => {
+    //   if (pathToRegexp(key).test(pathname)) {
+    //     currRouterData = this.breadcrumbNameMap[key];
+    //   }
+    // });
+    if (!currRouterData) {
+      return 'vipBocai';
+    }
+    const message = formatMessage({
+      id: currRouterData.locale || currRouterData.name,
+      defaultMessage: currRouterData.name,
+    });
+    return `${message} - vipBocai`;
+  };
+
   render() {
     const { children } = this.props;
+    const {
+      location: { pathname },
+    } = this.props;
     return (
       // @TODO <DocumentTitle title={this.getPageTitle()}>
-      <div className={styles.container}>
-        <div className={styles.content}>
-          <div className={styles.top}>
-            <div className={styles.header}>
-              <Link to="/">
-                <img alt="logo" className={styles.logo} src={logo}/>
-                <span className={styles.title}>vipBocai</span>
-              </Link>
+      <React.Fragment>
+        <DocumentTitle title={this.getPageTitle(pathname)}>
+          <div className={styles.container}>
+            <div className={styles.content}>
+              <div className={styles.top}>
+                <div className={styles.header}>
+                  <Link to="/">
+                    <img alt="logo" className={styles.logo} src={logo}/>
+                    <span className={styles.title}>vipBocai</span>
+                  </Link>
+                </div>
+                <div className={styles.desc}>会员管理系统</div>
+              </div>
+              {children}
             </div>
-            <div className={styles.desc}>会员管理系统</div>
+            <GlobalFooter copyright={copyright}/>
           </div>
-          {children}
-        </div>
-        <GlobalFooter copyright={copyright}/>
-      </div>
+        </DocumentTitle>
+      </React.Fragment>
+
     );
   }
 }
